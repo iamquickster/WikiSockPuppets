b9bd73185898265d85885a8586858749582bbf94
quickster <feo.pascal@gmail.com>
2016-02-11 16:02:24 -0500

    Changed to one file setup to contain Mapper and Reducer

--- WikiSockPuppets/src/wikisockpuppets/MonthInvestigationMapper.java
+++
@@ -1,5 +0,0 @@
-package wikisockpuppets;
-
-public class MonthInvestigationMapper {
-
-}
---
+++ WikiSockPuppets/src/wikisockpuppets/WikiSockPuppetHisto.java
@@ -0,0 +1,10 @@
+package wikisockpuppets;
+
+public class WikiSockPuppetHisto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
